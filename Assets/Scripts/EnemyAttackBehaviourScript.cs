66dd8350af457b0bb46e6be6e62e3ee949049585
Brionna Franklin <54289307+brionnafranklin@users.noreply.github.com>
2020-06-24 18:04:27 -0500

    Made major progress on health bar
    still need to fix a few errors but the health bar is close to being done

--- Assets/Scripts/EnemyAttackBehaviourScript.cs
+++ Assets/Scripts/EnemyAttackBehaviourScript.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class EnemyAttackBehaviourScript : MonoBehaviour
 {
     public float towerHealth = 100;
+    public HealthBarBehaviourScript healthBar;
     // Start is called before the first frame update
     void Start()
     {
-        
+        healthBar.SetMaxHealth(towerHealth);
     }
 
     // Update is called once per frame
