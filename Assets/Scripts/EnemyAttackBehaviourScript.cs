a69390e566674c69587027ba3c288bcca980236b
Brionna Franklin <54289307+brionnafranklin@users.noreply.github.com>
2020-06-26 08:52:19 -0500

    Connected (broken) health bar to tower
    health bar max gets set to current health for som reason

--- Assets/Scripts/EnemyAttackBehaviourScript.cs
+++ Assets/Scripts/EnemyAttackBehaviourScript.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class EnemyAttackBehaviourScript : MonoBehaviour
 {
     public float towerMaxHealth = 100;
-    public float towerCurrentHealth = 100;
+    public float towerCurrentHealth;
     public HealthBarBehaviourScript healthBar;
     // Start is called before the first frame update
     void Start()
